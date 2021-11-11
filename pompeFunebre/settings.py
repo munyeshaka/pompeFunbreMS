8d44051134bd0fda17d36fb27ec2533185a47a4a
munyeshaka <aimablemunyeshaka30@gmail.com>
2021-11-11 11:59:57 +0200

    some stuff and adding products

--- pompeFunebre/settings.py
+++ pompeFunebre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'pompeFunebre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            
         },
     },
 ]
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/image/download/"
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import * #Products, Category, Customer, Order
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 # Register your models here.
+admin.site.register(Products,AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+
+# username = Tanushree, email = tanushree7252@gmail.com, password = 1234
+# username = munyeshaka, email = muai@biu.bi, password = Aima_8080
